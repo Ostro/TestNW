42e6be619f723433281c599123dec5e5e28005ed
Arnaud Lebreton <arnaud.lebreton@fullsix.com>
2016-01-14 12:05:53 +0100

    add grunt task to produces releases of the app

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,9 +8,33 @@ module.exports = function (grunt) {
 
     // configurable paths
     var config = {
+        name: 'appName',
         app: 'app',
-        dist: 'dist'
+        dist: 'dist',
+        release: 'release',
+        version: 'v' + require('./package.json').version,
+        platforms: ['linux64', 'linux32', 'osx64', 'osx32', 'win64', 'win32']
     };
+    config.distrib = config.dist + '/' + config.name + ' - ' + config.version;
+
+    var compressConfig = {};
+    // define task compress:dist regarding pltaforms list
+    config.platforms.forEach(function(platform) {
+        compressConfig[platform] = {
+            options: {
+                archive: config.release + '/' + config.name + '-' + config.version + '-' + platform + '.zip',
+                mode: 'zip'
+            },
+            files: [
+                {
+                    expand: true,
+                    cwd: '<%= config.distrib %>/' + platform + '/',
+                    src: ['**'],
+                    dest: config.name + '-' + config.version + '-' + platform + '/'
+                }
+            ]
+        }
+    });
 
     grunt.initConfig({
         config: config,
@@ -32,7 +56,7 @@ module.exports = function (grunt) {
                 platforms: ['linux', 'osx', 'win'],
                 build_dir: '<%= config.dist %>/',
                 buildType: 'versioned',
-                appName: 'EVK'
+                appName: config.name
             },
             src: ['./app/**/*']
         },
@@ -42,13 +66,30 @@ module.exports = function (grunt) {
                 option: {
                     force: true
                 }
+            },
+            release: {
+                src: '<%= config.release %>/',
+                option: {
+                    force: true
+                }
             }
-        }
+        },
+        compress: compressConfig
     });
 
     grunt.registerTask('build', [
         'jshint',
         'clean:dist',
         'nwjs'
-    ])
+    ]);
+
+    grunt.registerTask('package', [
+        'clean:release',
+        'compress'
+    ]);
+
+    grunt.registerTask('release', [
+        'build',
+        'package'
+    ]);
 };
